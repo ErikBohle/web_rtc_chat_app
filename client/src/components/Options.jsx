67f994e560df1902026bf0d75bc3676826e494a3
erikbohle <55728992+ErikBohle@users.noreply.github.com>
2021-04-20 17:30:30 +0200

    barebones setup of file

---
+++ client/src/components/Options.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Options = ({children}) => {
+  return (
+    <div>
+
+      Notifications
+      {children}
+    </div>
+  )
+}
+
+export default Options
---
+++ client/src/components/VideoPlayer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Grid, Typography, Paper} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles';
+
+import {socketContext} from '../SocketContext'
+
+const Videoplayer = () => {
+  return (
+    <div>
+      Videochat
+    </div>
+  )
+}
+
+export default Videoplayer
